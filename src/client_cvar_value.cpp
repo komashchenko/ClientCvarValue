29aae5c7a7930a8a3b3e71a556930be977c78e84
Kyle <kyle@kxnrl.com>
2024-03-27 02:02:54 +0800

    fix: incorrect offset of windows (#1)

--- src/client_cvar_value.cpp
+++ src/client_cvar_value.cpp
@@ -23,7 +23,7 @@
 
 constexpr int CLIENTLANGUAGEID = INT_MAX;
 constexpr int CLIENTOPERATINGSYSTEMID = INT_MAX - 1;
-constexpr int ProcessRespondCvarValueOffset = WIN_LINUX(39, 40);
+constexpr int ProcessRespondCvarValueOffset = WIN_LINUX(38, 40);
 constexpr int ClientSlotOffset = WIN_LINUX(208, 224);
 constexpr int SendNetMessageOffset = WIN_LINUX(34, 35);
 
