560ded149ebb4cbc65ec136a8941b9b1eeeede88
komashchenko <komashchenko@users.noreply.github.com>
2024-06-12 23:12:49 +0300

    Update ProcessRespondCvarValue offset

--- src/client_cvar_value.cpp
+++ src/client_cvar_value.cpp
@@ -23,7 +23,7 @@
 
 constexpr int CLIENTLANGUAGEID = INT_MAX;
 constexpr int CLIENTOPERATINGSYSTEMID = INT_MAX - 1;
-constexpr int ProcessRespondCvarValueOffset = WIN_LINUX(38, 40);
+constexpr int ProcessRespondCvarValueOffset = WIN_LINUX(39, 41);
 constexpr int ClientSlotOffset = WIN_LINUX(72, 88);
 
 ClientCvarValue g_ClientCvarValue;
